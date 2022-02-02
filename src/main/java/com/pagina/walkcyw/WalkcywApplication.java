a11a5bbd87d552c7810046ff63f3faa0f2bcdaf9
her <her.laplata@gmail.com>
2022-02-02 03:35:34 -0300

    Creacion del proyecto y archivo index.html

---
+++ src/main/java/com/pagina/walkcyw/WalkcywApplication.java
@@ -0,0 +1,13 @@
+package com.pagina.walkcyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkcywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkcywApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagina/walkcyw/WalkcywApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagina.walkcyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkcywApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
