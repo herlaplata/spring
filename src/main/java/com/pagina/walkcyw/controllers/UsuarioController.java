20aba7526c5e6496d669207c3a229c1b9ba8f6e4
herlaplata <her.laplata@gmail.com>
2022-02-04 04:33:47 -0300

    Se agrego bootstrap y se creo un controlador

---
+++ src/main/java/com/pagina/walkcyw/controllers/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.pagina.walkcyw.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "productos")
+    public List<String> productos () {
+        return List.of("manzana", "Pera");
+
+    }
+}
